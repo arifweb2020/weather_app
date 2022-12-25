46e8e245da0c05edfa756b03fce81bd18d93ec5a
arifweb2020 <arifhussainb2019@gmail.com>
2022-12-25 21:09:13 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -64,6 +64,40 @@ function App() {
 
   function getIconUrl(iconCode) {
     console.log(iconCode)
+    return iconCode === 0 ? <img src={sun} alt='img' width='100' /> :
+      iconCode === 1 ? <img src={sun} alt='img' width='100' /> :
+        iconCode === 2 ? <img src={cloudSun} alt='img' width='100' /> :
+          iconCode === 3 ? <img src={cloud} alt='img' width='100' /> :
+            iconCode === 45 ? <img src={smoke} alt='img' width='100' /> :
+              iconCode === 48 ? <img src={smoke} alt='img' width='100' /> :
+                iconCode === 51 ? <img src={showers} alt='img' width='100' /> :
+                  iconCode === 53 ? <img src={showers} alt='img' width='100' /> :
+                    iconCode === 55 ? <img src={showers} alt='img' width='100' /> :
+                      iconCode === 56 ? <img src={showers} alt='img' width='100' /> :
+                        iconCode === 57 ? <img src={showers} alt='img' width='100' /> :
+                          iconCode === 61 ? <img src={showers} alt='img' width='100' /> :
+                            iconCode === 63 ? <img src={showers} alt='img' width='100' /> :
+                              iconCode === 65 ? <img src={showers} alt='img' width='100' /> :
+                                iconCode === 66 ? <img src={showers} alt='img' width='100' /> :
+                                  iconCode === 67 ? <img src={showers} alt='img' width='100' /> :
+                                    iconCode === 80 ? <img src={showers} alt='img' width='100' /> :
+                                      iconCode === 81 ? <img src={showers} alt='img' width='100' /> :
+                                        iconCode === 82 ? <img src={showers} alt='img' width='100' /> :
+                                          iconCode === 71 ? <img src={snowflake} alt='img' width='100' /> :
+                                            iconCode === 73 ? <img src={snowflake} alt='img' width='100' /> :
+                                              iconCode === 75 ? <img src={snowflake} alt='img' width='100' /> :
+                                                iconCode === 77 ? <img src={snowflake} alt='img' width='100' /> :
+                                                  iconCode === 85 ? <img src={snowflake} alt='img' width='100' /> :
+                                                    iconCode === 86 ? <img src={snowflake} alt='img' width='100' /> :
+                                                      iconCode === 95 ? <img src={bolt} alt='img' width='100' /> :
+                                                        iconCode === 96 ? <img src={bolt} alt='img' width='100' /> :
+                                                          iconCode === 99 ? <img src={bolt} alt='img' width='100' /> :
+                                                            <img src={sun} alt='img' width='100' />
+  }
+
+
+  function getSmallIconUrl(iconCode) {
+    console.log(iconCode)
     return iconCode === 0 ? <img src={sun} alt='img' width='50' /> :
       iconCode === 1 ? <img src={sun} alt='img' width='50' /> :
         iconCode === 2 ? <img src={cloudSun} alt='img' width='50' /> :
@@ -95,16 +129,43 @@ function App() {
                                                             <img src={sun} alt='img' width='50' />
   }
 
+
+
+  //const milliseconds = 1575909015000
+
+  // const dateObject = new Date(milliseconds)
+
+  // const humanDateFormat = dateObject.toLocaleString() //2019-12-9 10:30:15
+
+  // dateObject.toLocaleString("en-US", {weekday: "long"}) // Monday
+  // dateObject.toLocaleString("en-US", {month: "long"}) // December
+  // dateObject.toLocaleString("en-US", {day: "numeric"}) // 9
+  // dateObject.toLocaleString("en-US", {year: "numeric"}) // 2019
+  // dateObject.toLocaleString("en-US", {hour: "numeric"}) // 10 AM
+  // dateObject.toLocaleString("en-US", {minute: "numeric"}) // 30
+  // dateObject.toLocaleString("en-US", {second: "numeric"}) // 15
+  // dateObject.toLocaleString("en-US", {timeZoneName: "short"}) // 12/9/2019, 10:30:15 AM CST
+
+  const dateObject = (mils) => {
+    const x = new Date(mils * 1000)
+    return x.toLocaleString("en-US", { weekday: "long" })
+  }
+
+  const indexNum = [0, 1, 2, 3, 4, 5, 6]
+
   return (
     <>
       {
         loading ? "plz wait" : <div className='container mt-5'>
-          <h1 className='text-center'>Weather App </h1>
-          <div>
-            <h1 className='text-center'><span>{getIconUrl(current.weathercode)} </span> {Math.round(current.temperature) || "loading.."}&deg;</h1>
-          </div>
-          <div className='row'>
-            <div className='col-md-3'>
+          {/* <h1 className='text-center'>Weather App </h1> */}
+
+          <div className='row topContainer' >
+
+            <div className='col-md-6 '>
+              <h1 ><span>{getIconUrl(current.weathercode)} </span> {Math.round(current.temperature)}&deg;C</h1>
+              <div className='headerLeft'></div>
+            </div>
+            <div className='col-md-2'>
               <div>
                 High<br /> {Math?.round(Math?.max(...daily?.temperature_2m_max)) || "-"}
               </div>
@@ -112,7 +173,7 @@ function App() {
                 Low<br /> {Math?.round(Math.min(...daily?.temperature_2m_min)) || "-"}
               </div>
             </div>
-            <div className='col-md-3'>
+            <div className='col-md-2'>
               <div>
                 FL High<br /> {Math.round(Math.max(...daily?.apparent_temperature_max)) || "-"}
               </div>
@@ -120,12 +181,50 @@ function App() {
                 FL Low<br /> {Math.round(Math.min(...daily?.apparent_temperature_min)) || "-"}
               </div>
             </div>
-            <div className='col-md-3'>
+            <div className='col-md-2'>
               <div>
                 Wind<br /> {current?.windspeed} mph
               </div>
 
             </div>
+
+
+            <div className='daily'>
+              <div className='dailyImg'>
+                {
+                  daily?.weathercode?.map((ele) => {
+                    return <div>{getSmallIconUrl(ele)}</div>
+                  }
+                  )
+
+                }
+
+              </div>
+
+              <div className='dailyday'>
+                {
+                  daily?.time?.map((ele) => {
+                    return <div >{dateObject(ele)}</div>
+                  }
+                  )
+                }
+              </div>
+
+              <div className='dailyWeather'>
+                {
+                  daily?.temperature_2m_max?.map((ele) => {
+                    return <div >{Math.round(ele)}&deg;C</div>
+                  }
+                  )
+                }
+              </div>
+
+
+
+
+            </div>
+
+
           </div>
 
         </div>
